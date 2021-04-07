120cb949c9ece420224985c520fe4c1acb95dcdc
jb <jasontbrown99@gmail.com>
2021-04-07 11:45:11 -0700

    add validation for user roles

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { checkLoggedIn } = require("../../middleware/auth");
+const { grantAccess } = require("../../middleware/roles");
 let router = express.Router();
 require("dotenv").config();
 
@@ -57,10 +58,19 @@ router.route("/signin").post(async (req, res) => {
   }
 });
 
-router.route("/profile").get(checkLoggedIn, async (req, res) => {
-  console.log(req.user);
-  res.status(200).send("ok");
-});
+router
+  .route("/profile")
+  .get(checkLoggedIn, grantAccess("readOwn", "profile"), async (req, res) => {
+    try {
+      const permission = res.locals.permission;
+      const user = await User.findById(req.user._id);
+      if (!user) return res.status(400).json({ message: "User not found" });
+
+      res.status(200).json(permission.filter(user._doc));
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  });
 
 const getUserProps = (user) => {
   return {

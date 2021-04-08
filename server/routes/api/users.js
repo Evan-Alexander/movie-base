ea5d161b0579dc2b2ba13718adafdc932d4d426a
jb <jasontbrown99@gmail.com>
2021-04-08 15:15:36 -0700

    update user profile and email routes

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -70,7 +70,74 @@ router
     } catch (error) {
       return res.status(400).send(error);
     }
-  });
+  })
+  .patch(
+    checkLoggedIn,
+    grantAccess("updateOwn", "profile"),
+    async (req, res) => {
+      try {
+        const user = await User.findOneAndUpdate(
+          { _id: req.user._id },
+          {
+            $set: {
+              firstname: req.body.firstname,
+              lastname: req.body.lastname,
+              age: req.body.age,
+            },
+          },
+          // Return the updated user object instead of the default original user object
+          { new: true }
+        );
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        res.status(200).json(getUserProps(user));
+      } catch (error) {
+        res.status(400).json({ message: "Problem updating", error: error });
+      }
+    }
+  );
+
+router.route("/isauth").get(checkLoggedIn, async (req, res) => {
+  res.status(200).send(getUserProps(req.user));
+});
+
+router
+  .route("/update_email")
+  .patch(
+    checkLoggedIn,
+    grantAccess("updateOwn", "profile"),
+    async (req, res) => {
+      try {
+        if (await User.emailTaken(req.body.newemail)) {
+          return res
+            .status(400)
+            .json({ message: "Sorry that email is already in use." });
+        }
+
+        const user = await User.findOneAndUpdate(
+          {
+            _id: req.user._id,
+            email: req.body.email,
+          },
+          {
+            $set: {
+              email: req.body.newemail,
+            },
+          },
+          { new: true }
+        );
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        const token = user.generateToken();
+        res
+          .cookie("flickbase-token", token)
+          .status(200)
+          .send({ email: user.email });
+      } catch (error) {
+        res.status(400).json({ message: "Problem updating", error: error });
+      }
+    }
+  );
 
 const getUserProps = (user) => {
   return {

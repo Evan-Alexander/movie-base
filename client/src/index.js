aaf652538e682255f6f81bbc655d58346cc6a8c8
jb <jasontbrown99@gmail.com>
2021-04-09 14:38:36 -0700

    add basic redux setup and routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import reportWebVitals from "./reportWebVitals";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
+
+import { Provider } from "react-redux";
+import ReduxStore from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ReduxStore()}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/store/reducers/notifications_reducer.js
@@ -0,0 +1,6 @@
+export default function notificationREducer(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
